87b9dc7289ddaa871700d1c44fb22cec2f8d0384
Flavio Camardella <flaviocamardella@MacBook-Pro-di-Flavio.local>
2017-11-27 17:47:43 +0100

    no message

--- inc/works.php
+++ inc/works.php
@@ -7,37 +7,37 @@
 			<div class="row">
 				<div class="col-md-12">
 					<div class="form-group">
-						<label for="subj1">
-						<input type="radio" name="subject" id="graphic" value="graphics">Graphics
-					</label>
-					<label for="subj2">
-						<input type="radio" name="subject" id="video" value="videos">Clips
-					</label>
-					<label for="subj3">
-						<input type="radio" name="subject" id="web" value="webs">Websites
-					</label>
-					<label for="subj4">
-						<input type="radio" name="subject" id="interaction" value="interactions">Interaction Design
-					</label>
+						<div class="center_works">
+							<label for="subj1">
+								<input type="radio" name="subject" id="graphic" value="graphics">Graphics
+							</label>
+							<label for="subj2">
+								<input type="radio" name="subject" id="video" value="videos">Clips
+							</label>
+							<label for="subj3">
+								<input type="radio" name="subject" id="web" value="webs">Websites
+							</label>
+							<label for="subj4">
+								<input type="radio" name="subject" id="interaction" value="interactions">Interaction Design
+							</label>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		
 	</div>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-4">
+	<div class="row">
+		<div class="col-md-4">
 				
-			</div>
-			<div class="col-md-4">
-				
-			</div>
-			<div class="col-md-4">
-				
-			</div>
 		</div>
-		<div class="row">
+		<div class="col-md-4">
+				
+		</div>
+		<div class="col-md-4">
+				
+		</div>
+		</div>
+	<div class="row">
 			<div class="col-md-3">
 				
 			</div>
