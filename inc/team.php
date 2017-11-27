7b20bd44806352a5d37694415c22cfd8cace8186
Francesca Piccoli <francescapiccoli@MacBook-Pro-di-Francesca.local>
2017-11-27 17:30:31 +0100

    struttura parte team e primo approccio slider javascript

--- inc/team.php
+++ inc/team.php
@@ -1,3 +1,104 @@
 <section id="team">
 	<!-- Team content -->
+
+	<div class="arrow arrow_right">
+	</div>
+
+	<div class="arrow arrow_left">
+	</div>
+
+	<div class="conteiner slide_1">
+
+		<div class="row">
+
+			<div class="col-md-4 col-12">
+				<div class="box_image 1_member">
+				</div>
+
+				<h2 class="name_member">
+					Valentina Manfredi
+				</h2>
+				<span class="team_role">
+					Project Manager
+				</span>
+			</div>
+
+			<div class="col-md-4 col-12">
+					<h2 class="name_member">
+						Francesca Piccoli
+					</h2>
+					<span class="team_role">
+						Front-end developer
+					</span>
+
+					<div class="box_image 2_member">
+					</div>
+			</div>
+
+			<div class="col-md-4 col-12">
+				<div class="box_image 3_member">
+				</div>
+
+				<h2 class="name_member">
+					Riccardo Zianna
+				</h2>
+				<span class="team_role">
+					UX/UI designer
+				</span>
+			
+			</div>
+
+		</div>
+
+
+	</div>
+
+
+
+	<div class="conteiner slide_2">
+
+		<div class="row">
+
+			<div class="col-md-4 col-12">
+				<div class="box_image 1_member">
+				</div>
+
+				<h2 class="name_member">
+					Ciccio
+				</h2>
+				<span class="team_role">
+					Project Manager
+				</span>
+			</div>
+
+			<div class="col-md-4 col-12">
+					<h2 class="name_member">
+						Pasticcio
+					</h2>
+					<span class="team_role">
+						Front-end developer
+					</span>
+
+					<div class="box_image 2_member">
+					</div>
+			</div>
+
+			<div class="col-md-4 col-12">
+				<div class="box_image 3_member">
+				</div>
+
+				<h2 class="name_member">
+					Riccardo Zianna
+				</h2>
+				<span class="team_role">
+					UX/UI designer
+				</span>
+			
+			</div>
+
+		</div>
+
+
+	</div>
+
 </section>
