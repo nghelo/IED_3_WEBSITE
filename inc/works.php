d1063d1cd5333493f67a2195b20f803840670b07
Flavio Camardella <flaviocamardella@MacBook-Pro-di-Flavio.local>
2017-11-27 17:32:11 +0100

    Sezione dei lavori

--- inc/works.php
+++ inc/works.php
@@ -1,3 +1,53 @@
 <section id="works">
+	<div class="title_container">
+		<h1 class="title">Our Works</h1>
+	</div>
+	<div class="box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="form-group">
+						<label for="subj1">
+						<input type="radio" name="subject" id="graphic" value="graphics">Graphics
+					</label>
+					<label for="subj2">
+						<input type="radio" name="subject" id="video" value="videos">Clips
+					</label>
+					<label for="subj3">
+						<input type="radio" name="subject" id="web" value="webs">Websites
+					</label>
+					<label for="subj4">
+						<input type="radio" name="subject" id="interaction" value="interactions">Interaction Design
+					</label>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				
+			</div>
+			<div class="col-md-4">
+				
+			</div>
+			<div class="col-md-4">
+				
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-3">
+				
+			</div>
+			<div class="col-md-3">
+				
+			</div>
+			<div class="col-md-6">
+				
+			</div>
+		</div>
+	</div>
 	<!-- Works content -->
 </section>
