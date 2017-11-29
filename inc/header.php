b4e204da27259ff07a6bcc86393b8271fb91e786
Peppe <giuseppe.mannino88@gmail.com>
2017-11-29 17:38:18 +0100

    no message

--- inc/header.php
+++ inc/header.php
@@ -3,4 +3,16 @@
 		<!-- Nav content -->
 	</nav>
 	<!-- Header content -->
+	<section id="header">
+		<div class="arrow_bt" id="arrow_bottom">	
+		</div>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-12 col-12" id="col_logo"> <!-- col-12 per logo-->
+						<h2 class="slogan">Slogan da aggiungere</h2><!-- spazio per logo-->
+						<div class="box_imgHeader"></div>
+					</div>
+				</div>
+			</div>
+	</section>
 </header>
