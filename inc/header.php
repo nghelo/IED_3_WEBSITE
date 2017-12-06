4314b6089e75a98e66ac03c758e2f8cbbc36a29b
Peppe <giuseppe.mannino88@gmail.com>
2017-12-06 18:01:51 +0100

    no message

--- inc/header.php
+++ inc/header.php
@@ -4,15 +4,19 @@
 	</nav>
 	<!-- Header content -->
 	<section id="header">
-		<div class="arrow_bt" id="arrow_bottom">	
-		</div>
-			<div class="container-fluid">
-				<div class="row">
-					<div class="col-md-12 col-12" id="col_logo"> <!-- col-12 per logo-->
-						<h2 class="slogan">Slogan da aggiungere</h2><!-- spazio per logo-->
-						<div class="box_imgHeader"></div>
+			<div class="slogan_container">
+				<h1 class="title">Spazio di testo per inserire uno slogan/motto
+				</h1>
+			</div>
+				<div class="img_container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="scroll_container">
+								<div class="riquadro_scroll">
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
 	</section>
 </header>
