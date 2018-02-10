48aaec9721a755be7218eb0d6774b694716e50a1
Flavio Camardella <flaviocamardella@MBP-di-Flavio.homenet.telecomitalia.it>
2018-02-10 11:41:53 +0100

    no message

--- inc/works.php
+++ inc/works.php
@@ -22,7 +22,7 @@
 									<input type="radio" name="subject" id="interaction" value="interactions">Interaction Design
 								</label>
 								<label for="subj4">
-									<input type="radio" name="subject" id="All" value="alls">All
+									<input checked type="radio" name="subject" id="All" value="alls">All
 								</label>
 							</div>
 						</div>
@@ -35,7 +35,7 @@
 			 <div class="col-md-4">
 		 	  <div class="video">
 		 	  	<div class="description">
-		 	  		<h2 class="project">ciioaoao</h2>
+		 	  		<h2 class="project"></h2>
 		 	  		<h3 class="subject"></h3>
 		 	  	</div>
 		 	  	<div class="photo"></div>
@@ -149,6 +149,8 @@
 			</div>
 			<div class="row">
 				<div class="space"></div>
+				
+				</a>
 			</div>
 		</div>
   </div>
