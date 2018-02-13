1c51f1e0c87a33c16343d18f32c3c428af25b4e2
rory2033 <33694014+rory2033@users.noreply.github.com>
2018-02-13 19:14:03 +0100

    Update works.js

--- js/works.js
+++ js/works.js
@@ -164,3 +164,23 @@ function positionOriginal() {
        $(".video").css("transform","translate(0px, 0px)");
        $(".interaction").css("transform","translate(0px, 0px)");
 }
+
+
+
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
